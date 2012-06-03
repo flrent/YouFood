282b9d4eff1a0582b73dae511886d85dcbf293db
koulibaly.papus@gmail.com <Papus@Papus-PC.(none)>
2012-06-03 09:04:32 +0200

    update paps

---
+++ public/admin/dist/debug/templates.js
@@ -0,0 +1,5 @@
+this['JST'] = this['JST'] || {};
+
+this['JST']['app/templates/accueil.html'] = function(data) { return function (obj,_) {
+var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<div class="row">\r\n\t<div class="span12">\r\n\t\t<h2>Bienvenue sur l\'interface d\'administration YouFood.</h2>\r\n\t</div>\r\n</div>\r\n');}return __p.join('');
+}(data, _)};
